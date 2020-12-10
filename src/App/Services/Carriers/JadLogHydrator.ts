70bd89cd3002b02c61b40821133d8cd6282af9a5
huandersonmachado <huandersonmachado@gmail.com>
2020-12-10 20:18:25 +0000

    conversão de classe para interface FreightFormat

--- src/App/Services/Carriers/JadLogHydrator.ts
+++ src/App/Services/Carriers/JadLogHydrator.ts
@@ -2,12 +2,19 @@ import FreightFormat from './../../Support/FreightFormat';
 
 export default class JadLogHydrator {
     parse(data: Object) {
-        const format = new FreightFormat();
-            format.ZipCodeEnd = '';
-            format.absoluteMoneyCost = '';
-            format.country = '';
-            format.polygonName = '';
-            format.weightStart = '';
-        return format;
+        const format: FreightFormat = {
+            zipCodeZtart: 'String',
+            ZipCodeEnd: 'String',
+            polygonName: 'String',
+            weightStart: 'String',
+            weightEnd: 'String',
+            absoluteMoneyCost: 'String',
+            pricePercent: 'String',
+            maxVolume: 'String',
+            timeCost: 'String',
+            country: 'String',
+            minimumValueInsurance: 'String',
+        }
+        return format
     }
 }
--- src/App/Support/FreightFormat.ts
+++ src/App/Support/FreightFormat.ts
@@ -1,13 +1,13 @@
-export default class FreightFormat {
-    public zipCodeZtart!: String;
-    public ZipCodeEnd!: String;
-    public polygonName!: String;
-    public weightStart!: String;
-    public weightEnd!: String;
-    public absoluteMoneyCost!: String;
-    public pricePercent!: String;
-    public maxVolume!: String;
-    public timeCost!: String;
-    public country!: String;
-    public minimumValueInsurance!: String;
+export default interface FreightFormat {
+    zipCodeZtart: String;
+    ZipCodeEnd: String;
+    polygonName: String;
+    weightStart: String;
+    weightEnd: String;
+    absoluteMoneyCost: String;
+    pricePercent: String;
+    maxVolume: String;
+    timeCost: String;
+    country: String;
+    minimumValueInsurance: String;
 }
